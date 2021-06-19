085aabbbc0c19a65259f48882caabd51857614a3
yuki <yuki.onishi1129@gmail.com>
2021-06-20 00:56:04 +0900

    add: AppContext

--- native/.eslintrc.js
+++ native/.eslintrc.js
@@ -18,7 +18,7 @@ module.exports = {
   },
   plugins: ['react', '@typescript-eslint', 'react-hooks'],
   rules: {
-    indent: ['error', 2],
+    // indent: ['error', 2],
     'linebreak-style': ['error', 'unix'],
     quotes: ['error', 'single'],
     semi: ['error', 'always'],
