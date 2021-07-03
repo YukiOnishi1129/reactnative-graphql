f87dd099f501e64cf6de742634fdd7efa7522096
yuki <yuki.onishi1129@gmail.com>
2021-07-04 03:29:13 +0900

    add: create todo mutation

--- web/src/components/templates/Home/index.tsx
+++ web/src/components/templates/Home/index.tsx
@@ -4,11 +4,42 @@
  * @copyright Yuki Onishi
  */
 import React from "react";
+import Box from "@material-ui/core/Box";
+/* components */
+import { AddTodo } from "./organisms/AddTodo";
+/* hooks */
+import {
+  useCreateTodoMutation,
+  useDoneTodoMutation,
+  useActiveTodoMutation,
+  useDeleteTodoMutation,
+} from "@/hooks/useGraphQL";
+/* types */
+import { EventType } from "@/types/event";
+/* styles */
+import { useStyles } from "./style";
 
 /**
  * HomeTemplate
  * @returns
  */
 export const HomeTemplate: React.VFC = () => {
-  return <p>HomeTemplate</p>;
+  /* styles */
+  const classes = useStyles();
+
+  return (
+    <div className={classes.area}>
+      <h2>TodoList</h2>
+      <Box
+        className={classes.main}
+        display="flex"
+        justifyContent="space-around"
+      >
+        <div className={classes.leftBar}>
+          <AddTodo />
+        </div>
+        <div className={classes.rightBar}></div>
+      </Box>
+    </div>
+  );
 };
--- web/src/constants/navigation.ts
+++ web/src/constants/navigation.ts
@@ -8,12 +8,12 @@
  * ナビゲーションの遷移先
  */
 export const NAVIGATION_LINK = {
-  TOP: "./",
-  SIGN_IN: "./signin",
-  SIGN_UP: "./signup",
-  HOME: "./home",
-  TODO_DETAIL: "./todo-detail",
-  MY_PAGE: "./mypage",
+  TOP: "/",
+  SIGN_IN: "/signin",
+  SIGN_UP: "/signup",
+  HOME: "/home",
+  TODO_DETAIL: "/todo-detail",
+  MY_PAGE: "/mypage",
 };
 
 /**
